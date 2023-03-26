b4e249392205ced46d24c6bec42a4e41093654bc
MarynaFundament <marina.fundament@ukr.net>
2023-03-27 00:21:11 +0300

    рои

--- src/countries-api.js
+++ src/countries-api.js
@@ -1,16 +1,19 @@
 const BASE_URL = `https://restcountries.com/#api-endpoints-v3-name`;
 
 
-export const fetchCountries = name => 
-fetch ('https://restcountries.com/v3.1/name/${name}')
-.then ((response) => {
-if(!response.ok){
-    throw new Error (response.status)
-}
+export const fetchCountries = name =>
+  fetch(`https://restcountries.com/v3.1/name/${name}`).then(response => {
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
 
-return response.json();
+    return response.json();
+  });
 
-})
+
+
+
+
 
 
 
