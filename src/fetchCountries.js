8135a56bf1058d207439fab660ace1178ee2b147
MarynaFundament <marina.fundament@ukr.net>
2023-03-27 14:46:27 +0300

    мам

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,7 @@
 export const fetchCountries = (name) => {
-  const url = `https://restcountries.com/v3.1/name/${name}`;
+  const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,languages,flags.svg;`
+
+
   return fetch(url)
     .then((response) => {
       if (!response.ok) {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,61 @@
-import './css/styles.css';
+// import './css/styles.css';
 import { fetchCountries } from './fetchCountries.js';
+// import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.getElementById("search-box");
+const wrapperList = document.querySelector(".country-list");
 const wrapperEl = document.querySelector(".country-info");
 
-searchBox.addEventListener(`input`, handleInputSubmit);
+
+
+searchBox.addEventListener(`input`, handleInputSubmit );
 
 function handleInputSubmit(event) {
   event.preventDefault();
-  const searchCity = event.target.value.trim();
+  const searchCountry = event.target.value.trim();
   
-  fetchCountries(searchCity).then(data => console.log(data)).catch(error => console.error(error));
+fetchCountries(searchCountry).then(data =>
+  createMarkupCountryInfo(data))
+   .catch(error => console.error(error));
+
+
 }
+
+function createMarkupCountryInfo(searchCountry) {
+  const markup = searchCountry
+    .map(({ name, capital, population, flags, languages }) => {
+      return `
+  <div class="country__flag">
+    
+    <p class="country__name">${name.official}</p>
+  </div>
+  <ul class="country__info">
+      <li class="country__item"> <b>Capital</b>:
+    <span class="country__span">${capital}</span>
+      </li>
+      <li class="country__item"> <b>Population</b>:
+    <span class="country__span">${population}</span>
+      </li>
+      <li class="country__item"> <b>Languages</b>:
+    <span class="country__span">${Object.values(languages).join(', ')}</span>
+      </li>
+  </ul>`;
+    })
+    .join('');
+
+  wrapperEl.insertAdjacentHTML('beforeend', markup);
+}
+
+
+
+
+
+
+
+
+
+
+
+  
