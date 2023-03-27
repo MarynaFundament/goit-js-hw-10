15fc586f51722a963d08d58a32a56342953e6127
MarynaFundament <marina.fundament@ukr.net>
2023-03-27 12:07:06 +0300

    hb

---
+++ src/fetchCountries.js
@@ -0,0 +1,22 @@
+export const fetchCountries = (name) => {
+  const url = `https://restcountries.com/v3.1/name/${name}`;
+  return fetch(url)
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      console.error(`Fetch error: ${error}`);
+    });
+};
+
+
+
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,22 +1,16 @@
 import './css/styles.css';
-import { fetchCountries } from './countries-api';
+import { fetchCountries } from './fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 
 const searchBox = document.getElementById("search-box");
-console.log(searchBox)
-
 const wrapperEl = document.querySelector(".country-info");
-console.log(wrapperEl)
 
-searchBox.addEventListener(`input`, handleInputSubmit)
+searchBox.addEventListener(`input`, handleInputSubmit);
 
-function handleInputSubmit(event){
-event.preventDefault();
-const searchedCity = event.target.value.trim()
-
-fetchCountries(searchedCity).then(data => {
-    console.log(data)
-})
-
+function handleInputSubmit(event) {
+  event.preventDefault();
+  const searchCity = event.target.value.trim();
+  
+  fetchCountries(searchCity).then(data => console.log(data)).catch(error => console.error(error));
 }
